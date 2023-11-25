a4997079d3be876bbfa42133500ec4cd6e5c962c
Nicolas Martyanoff <nicolas@n16f.net>
2023-11-25 17:13:04 +0100

    add utilities to generate solution source files

--- src/utils.lisp
+++ src/utils.lisp
@@ -9,7 +9,11 @@
    #:download-input-file
    #:input-file-path
    #:input-file-data
-   #:input-file-lines))
+   #:input-file-lines
+   #:format-solution-code
+   #:solution-file-path
+   #:generate-solution-file
+   #:open-problem-page))
 
 (in-package :aoc2023-utils)
 
@@ -154,3 +158,54 @@ if it does not exist locally."
     (let ((eol (or (position #\Newline data :start start :end end) end)))
       (push (subseq data start eol) lines)
       (setf start (1+ eol)))))
+
+(defun format-solution-code (day &key stream)
+  "Generate the initial code for a solution to a daily problem and write it to a
+stream."
+  (flet ((form (control &rest args)
+           (apply #'format stream control args)
+           (terpri stream)))
+    (form "~
+(defpackage :aoc~D-~2,'0D
+  (:use :cl :aoc~D-utils)
+  (:export
+   #:*lines*
+   #:solve-1
+   #:solve-2))"
+          *year* day *year*)
+    (form "~%(in-package :aoc~D-~2,'0D)" *year* day)
+    (form "~%(defvar *lines* (input-file-lines ~D))" day)
+    (form "~%~
+(defun solve-1 ()
+  ;; TODO
+  nil)")
+    (form "~%~
+(defun solve-2 ()
+  ;; TODO
+  nil)")))
+
+(defun solution-file-path (day)
+  "Return the absolute path of a solution file in the repository."
+  (let ((system (format nil "aoc~D" *year*))
+        (subpath (make-pathname :directory '(:relative "src")
+                                :name (format nil "day-~2,'0D" day)
+                                :type "lisp")))
+    (asdf:system-relative-pathname system subpath)))
+
+(defun generate-solution-file (day)
+  "Generate the code for the solution file and write it. Return the path of the
+file."
+  (let ((path (solution-file-path day)))
+    (with-open-file (file path
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+      (format-solution-code day :stream file))
+    path))
+
+(defun open-problem-page (day)
+  "Open the web page for a specific daily problem in Firefox."
+  (let* ((uri (format nil "https://adventofcode.com/~D/day/~D" *year* day))
+         (command `("firefox" ,uri)))
+    (uiop:run-program command :force-shell t :output nil :error-output t)
+    nil))
